dc176385c522dcad4d306fee8153455da98e7e93
archshift <admin@archshift.com>
2015-02-13 22:15:39 -0800

    AppVeyor: remove sanitizing, use git instead to make the filename

--- .make-appveyor-filename.js
+++
@@ -1,7 +0,0 @@
-var util = require('util');
-var sanitize = require("sanitize-filename");
-
-var dstFileName = process.env.APPVEYOR_REPO_COMMIT.substring(0, 8) + " - " + 
-                process.env.APPVEYOR_REPO_COMMIT_MESSAGE.substring(0, 100) + ".7z";
-console.log(sanitize(dstFileName));
-
